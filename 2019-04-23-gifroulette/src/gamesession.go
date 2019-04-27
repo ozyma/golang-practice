470377e9bfd42598eb05d6c2a1254732d807332b
to <ozyma@ozymastudios.com>
2019-04-26 20:53:39 -0400

    added changes to new files

--- 2019-04-23-gifroulette/src/gamesession.go
+++ 2019-04-23-gifroulette/src/gamesession.go
@@ -1,14 +1,34 @@
 package main
 
+import "fmt"
+
+/*GameSession will hold all the necessary information for a single lobby of
+players to connect and play a game
+
+*/
 type GameSession struct {
-	SessionID          string     `json:"sessionID"`
-	SessionCookieToken string     `json:"sessionCookieToken"`
-	SessionAdminToken  string     `json:"sessionAdminToken"`
-	PlayerCount        int        `json:"playerCount"`
+	SessionID          string     `json:"SessionID"`
+	SessionCookieToken string     `json:"SessionCookieToken"`
+	SessionAdminToken  string     `json:"SessionAdminToken"`
+	PlayerList         []Player   `json:"PlayerList"`
+	PlayerCount        int        `json:"PlayerCount"`
 	Questions          []Question `json:"questions"`
 }
 
 type Question struct {
 	Prompt  string `json:"prompt"`
 	GifLink string `json:"gifLink"`
+}
+
+type Player struct {
+	IP string `json:"IP"`
+}
+
+func (r GameSession) PrintGameSession() {
+	fmt.Println(r)
+}
+
+func (r GameSession) ChangeIDName(){
+	r.SessionID = "RogerWilko"
+	r.PrintGameSession()
 }
--- 2019-04-23-gifroulette/src/main.go
+++ 2019-04-23-gifroulette/src/main.go
@@ -2,34 +2,13 @@ package main
 
 import (
 	//"encoding/json"
-	"fmt"
+	//"fmt"
 	//"log"
 	//"net/http"
-	"math/rand"
+	//"math/rand"
 	//"time"
-	"strconv"
+	//"strconv"
 )
-
-//gameSession object will hold all of the necessary
-func generateNewGameSession() {
-	x := GameSession{
-		SessionID:          strconv.Itoa(rand.Intn(1000)),
-		SessionCookieToken: strconv.Itoa(rand.Intn(1000)),
-		SessionAdminToken:  strconv.Itoa(rand.Intn(1000)),
-		PlayerCount:        rand.Intn(8),
-		Questions: []Question{
-			{"testKey", "testVal"},
-			{"testKey2", "testVal2"},
-		},
-	}
-	fmt.Println("")
-	fmt.Println("=========================")
-	fmt.Println(x)
-	fmt.Println("=========================")
-	fmt.Println("")
-	
-
-}
 
 /*func indexhandler(w http.ResponseWriter, r *http.Request) {
 	mySession := GameSession{
@@ -51,9 +30,24 @@ func main() {
 	/*	http.HandleFunc("/", indexhandler)
 		http.ListenAndServe(":8080", nil)*/
 
-		for i:=0; i<=1000;i++{
-			generateNewGameSession()
+	DB := make(map[string]*GameSession)
+	
+	DB["rustygiraffe223"] = &GameSession{
+		SessionID:          "rustygiraffe223",
+		SessionCookieToken: "IblPL00sHz7emKLtztel1g==", //Key:tannerandjordan
+		SessionAdminToken:  "jwKeRVeWNS2n9N+1pYpFHg==", //Key:tannerandjordanadmin
+		PlayerList: []Player{
+			{"192.168.1.1"},
+		},
+		PlayerCount:        3,
+		Questions: []Question{
+			{"Why is your dog making this face?", "https://media.giphy.com/media/21GCae4djDWtP5soiY/giphy.gif"},
+			{"When you notice ________", "https://media.giphy.com/media/lJ0JGfNBrRWJVCRChd/giphy.gif"},
+			{"When your text from your Mom says ________","https://media.giphy.com/media/oOxBQwNqGwxeWLDF6A/giphy.gif"},
+		},
+	}
 
-		}
+	//fmt.Println(DB["rustygiraffe223"])
+	DB["rustygiraffe223"].ChangeIDName()
 
 }
